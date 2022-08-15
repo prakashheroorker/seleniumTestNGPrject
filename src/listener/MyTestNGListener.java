6d7061cbd69878327ebe9683c301b1f1542b8027
Prakash <prakashheroorker@gmail.com>
2022-08-15 22:16:02 +0530

    Second COMMIT

--- src/listener/MyTestNGListener.java
+++ src/listener/MyTestNGListener.java
@@ -12,8 +12,7 @@ public class MyTestNGListener implements ITestListener{
 		System.out.println(result.getName());
 		//System.out.println(result.getTestContext().getAttribute(name));
 		System.out.println(result.getStatus());
-		System.out.println("Checking the github updates");
-		System.out.println("Checking the github updates- third update");
+		System.out.println("Updates - Latest 1");
 		System.out.println(result.getThrowable().getMessage());
 	}
 	
